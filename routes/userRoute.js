2cafbe215fc998745577d1fff5d6106631da2fb3
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2024-08-20 11:36:55 -0700

    upload

---
+++ routes/userRoute.js
@@ -0,0 +1,9 @@
+import express from "express"
+import {fetch, create} from "../controller/userController.js"
+
+const route = express.Router();
+
+route.post("/create", create);
+route.get("/fetch", fetch);
+
+export default route;
