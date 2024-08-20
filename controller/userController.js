15314f1250c25a746caaaaaffeab41d7e32c0299
Sabrina Shafer <141285407+sshafe928@users.noreply.github.com>
2024-08-20 14:58:30 -0700

    update

--- controller/userController.js
+++ controller/userController.js
@@ -18,10 +18,48 @@ export const create = async (req, res) => {
     }
 }
 
+
+
 export const fetch = async (req, res) => {
     try {
-        return res.json("Hello world.");
+        const users = await User.find();
+        if(users.length === 0){
+            return res.status(404).json({message: "User not found."})
+        }
+        res.status(200).json(users)
     }catch (error) {
         res.status(500).json({error:"Internal Server error."});
+    }
+};
+
+
+
+export const update = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const userExist = await User.findOne({_id:id})
+        if (!userExist){
+            return res.status(404).json({message: "User not found"})
+        }
+        const updateUser = await User.findByIdAndUpdate(id, req.body,{new:true})
+        res.status(201).json(updateUser);
+    } catch (error) {
+        res.status(500).json({error:"Internal Server error."});
+    }
+};
+
+
+
+export const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const userExist = await User.findOne({ _id: id });
+        if (!userExist) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        await User.findByIdAndDelete(id);
+        res.status(200).json({ message: "User deleted successfully." });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error." });
     }
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,12 @@
 import express from "express"
-import {fetch, create} from "../controller/userController.js"
+import {fetch, create, update, deleteUser} from "../controller/userController.js"
 
 const route = express.Router();
 
 route.post("/create", create);
-route.get("/fetch", fetch);
+route.get("/getAllUsers", fetch);
+route.put("/update/:id", update);
+route.delete("/delete/:id", deleteUser);
+
 
 export default route;
